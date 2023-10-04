b9e6e47bec8600bab716ec402e7e7fbdc5056189
Habiba El-Sayed <76976297+habibaelsayed@users.noreply.github.com>
2023-10-04 13:54:32 -0700

    Create 3-chessboard.js

---
+++ chapter-2/3-chessboard.js
@@ -0,0 +1,11 @@
+let board = "";
+let size = Number(prompt("Enter grid size));
+for(let i=0;i<size;i++){
+    for(let j=0;j<size;j++){
+        if((j+i)%2==0)
+            board+=" ";
+        else
+            board+="#";
+    }
+    board+="\n";
+}
